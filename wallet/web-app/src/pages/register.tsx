b0566d1dcc120d65375ec1dbad30409d071edfa3
Jacob Scott Farmer <xboxteejaymcfarmer@gmail.com>
2026-01-12 20:48:37 -0700

    Create register.tsx

---
+++ wallet/web-app/src/pages/register.tsx
@@ -0,0 +1,10 @@
+import { Layout } from "../components/Layout";
+import { RegistrationFlow } from "../components/RegistrationFlow";
+
+export default function RegisterPage() {
+  return (
+    <Layout title="Registration">
+      <RegistrationFlow />
+    </Layout>
+  );
+}
