941e180de8d5f0b562e6063316accb3bdd5b6c07
Jacob Scott Farmer <xboxteejaymcfarmer@gmail.com>
2026-01-12 20:40:50 -0700

    Create index.ts

---
+++ services/charter-api/src/index.ts
@@ -0,0 +1,8 @@
+import { startServer } from "./server";
+import { config } from "@augmented-rights/services-common/src/config";
+
+startServer(config.port + 1).catch((err) => {
+  // eslint-disable-next-line no-console
+  console.error(err);
+  process.exit(1);
+});
