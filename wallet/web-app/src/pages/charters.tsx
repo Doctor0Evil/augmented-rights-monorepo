53a4cc387383b6c829c148de0d81180f9b516d57
Jacob Scott Farmer <xboxteejaymcfarmer@gmail.com>
2026-01-12 20:48:53 -0700

    Create charters.tsx

---
+++ wallet/web-app/src/pages/charters.tsx
@@ -0,0 +1,10 @@
+import { Layout } from "../components/Layout";
+import { CharterManager } from "../components/CharterManager";
+
+export default function ChartersPage() {
+  return (
+    <Layout title="Charter Management">
+      <CharterManager />
+    </Layout>
+  );
+}
